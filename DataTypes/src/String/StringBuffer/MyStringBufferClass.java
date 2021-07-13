2874375431a5ca76845f2e869571b5dd6105f6b6
ruslooob <rm952576@gmail.com>
2021-07-13 23:40:45 +0300

    added DataTypes Module with String classes

---
+++ DataTypes/src/String/StringBuffer/MyStringBufferClass.java
@@ -0,0 +1,35 @@
+package String.StringBuffer;
+
+public class MyStringBufferClass {
+    /* Можете изменить значение sb, чтобы еще потренироваться */
+    StringBuffer sb = new StringBuffer("Hello world");
+
+    public void m() {
+        // Hello world!!
+        System.out.println(sb.append("!!"));
+        // Hello, world!!
+        System.out.println(sb.insert(5, ","));
+        // world!!
+        System.out.println(sb.delete(0, 6));
+        // worl!!
+        System.out.println(sb.deleteCharAt(5));
+        // 7
+        System.out.println(sb.length());
+        // 2
+        System.out.println(sb.indexOf("o"));
+        // 2
+        System.out.println(sb.lastIndexOf("o"));
+        // !
+        System.out.println(sb.charAt(5));
+        // java!!
+        System.out.println(sb.replace(0, 5, "java"));
+        // 34
+        System.out.println(sb.compareTo( new StringBuffer("Hello")));
+        // !!avaj
+        System.out.println(sb.reverse());
+        // !avaj
+        System.out.println(sb.substring(1));
+        // !a
+        System.out.println(sb.substring(1, 3));
+    }
+}
---
+++ DataTypes/src/String/StringBuilder/MyStringBuilderClass.java
@@ -0,0 +1,35 @@
+package String.StringBuilder;
+
+public class MyStringBuilderClass {
+    /* Можете изменить значение sb, чтобы еще потренироваться */
+    StringBuilder sb = new StringBuilder("Hello world");
+    /* API анологично StringBuffer */
+    public void m() {
+        // Hello world!!
+        System.out.println(sb.append("!!"));
+        // Hello, world!!
+        System.out.println(sb.insert(5, ","));
+        //  world!!
+        System.out.println(sb.delete(0, 6));
+        //  worl!!
+        System.out.println(sb.deleteCharAt(5));
+        // 7
+        System.out.println(sb.length());
+        // 2
+        System.out.println(sb.indexOf("o"));
+        // 2
+        System.out.println(sb.lastIndexOf("o"));
+        // !
+        System.out.println(sb.charAt(5));
+        // java!!
+        System.out.println(sb.replace(0, 5, "java"));
+        // 34
+        System.out.println(sb.compareTo( new StringBuilder("Hello")));
+        // !!avaj
+        System.out.println(sb.reverse());
+        // !avaj
+        System.out.println(sb.substring(1));
+        // !a
+        System.out.println(sb.substring(1, 3));
+    }
+}
